28ac32817a7e62bab193d7885c09d1f1b8f00dad
Minwoo Kim <rlaalsdn456456@naver.com>
2019-12-12 17:01:29 +0900

    클래스 : 문자 내용 (추후 DB로)

--- app/src/main/java/com/example/fakeyolo/Content.java
+++ app/src/main/java/com/example/fakeyolo/Content.java
@@ -1,10 +1,18 @@
 package com.example.fakeyolo;
 
+import java.util.Random;
+
 public class Content {
-    private static final String[] schools = new String[]{ ("Ask me anything"),
+    private static final String[] content = new String[]{ ("Ask me anything"),
             ("Send me love"),
             ("Would you date me?"),
             ("Tell me a secret"),
             ("Your ops on me with emojis")};
+
+    public String getRandom(){
+        Random rand = new Random();
+        int rand_num = rand.nextInt(content.length);
+        return content[rand_num];
+    }
 
 }
