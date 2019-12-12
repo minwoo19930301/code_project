fd2883ff42321b7ba2157dbbba83412a421eec44
Minwoo Kim <rlaalsdn456456@naver.com>
2019-12-12 16:22:42 +0900

    클래스 : dice 버튼용 바뀔 텍스트 모음

---
+++ app/src/main/java/com/example/fakeyolo/Content.java
@@ -0,0 +1,10 @@
+package com.example.fakeyolo;
+
+public class Content {
+    private static final String[] schools = new String[]{ ("Ask me anything"),
+            ("Send me love"),
+            ("Would you date me?"),
+            ("Tell me a secret"),
+            ("Your ops on me with emojis")};
+
+}
